bb7d52439fdc131749c7d78b592e1334ee1f1059
pauloladapo13 <72332084+pauloladapo13@users.noreply.github.com>
2021-09-05 18:59:52 +0200

    Phone number location

---
+++ NumberLocation.py
@@ -0,0 +1,11 @@
+from phonenumbers import carrier
+import phonenumbers
+from Test import number
+
+from phonenumbers import geocoder
+
+ch_number = phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_number, "es"))
+
+service_number = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_number, "es"))
